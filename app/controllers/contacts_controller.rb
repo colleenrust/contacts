df7323aed67a4e9635896489024d6a77e168ba4d
Colleen <colleenrust524@EMAIL.com>
2023-03-29 11:56:27 -0500

    add jbuilder file, identifies contacts by id

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,11 @@
 class ContactsController < ApplicationController
-  def method
-    contact = Conact.first
-    render json: {
-      id: contact.id ,
-      first_name: contact.first_name,
-      last_name: contact.last_name,
-      email: contact.email,
-      phone_number: contact.phone_number
-
-    }
+  def show
+    @contact = Contact.find_by(id: 4)
+    render template: "contacts/show"
+      
+  end
+  def all 
+    @contacts = Contact.all
+    render template: "contacts/index"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/one_contact" => "contacts#first"
+  # get "/all_contacts" => ""
   # Defines the root path route ("/")
   # root "articles#index"
 end
